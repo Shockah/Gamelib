3e8ef3aa24670665edcdf8c4ac4ed9bda826687d
shockah <lolshockah@gmail.com>
2013-06-26 19:58:10 +0200

    Frame spacing in SpriteSheet

--- src/pl/shockah/glib/gl/SpriteSheet.java
+++ src/pl/shockah/glib/gl/SpriteSheet.java
@@ -5,17 +5,28 @@ import pl.shockah.glib.gl.tex.Texture;
 
 public class SpriteSheet extends TextureSupplier {
 	protected final Image[][] grid;
-	protected final int gridX, gridY;
+	protected final int gridX, gridY, spacingX, spacingY;
 	
+	public SpriteSheet(Texture tex, int grid) {
+		this(tex,grid,grid);
+	}
 	public SpriteSheet(Texture tex, int gridX, int gridY) {
+		this(tex,gridX,gridY,0);
+	}
+	public SpriteSheet(Texture tex, int gridX, int gridY, int spacing) {
+		this(tex,gridX,gridY,spacing,spacing);
+	}
+	public SpriteSheet(Texture tex, int gridX, int gridY, int spacingX, int spacingY) {
 		super(tex);
 		this.gridX = gridX;
 		this.gridY = gridY;
+		this.spacingX = spacingX;
+		this.spacingY = spacingY;
 		
 		int w = tex.getWidth(), h = tex.getHeight();
 		grid = new Image[w/gridX][h/gridY];
 		
-		for (int y = 0; y < h; y += gridY) for (int x = 0; x < w; x += gridX) {
+		for (int y = 0; y < h; y += gridY+spacingY) for (int x = 0; x < w; x += gridX+spacingX) {
 			grid[x][y] = new Image2(tex,x,y);
 		}
 	}
@@ -36,7 +47,7 @@ public class SpriteSheet extends TextureSupplier {
 		}
 		
 		public Rectangle getTextureRect() {
-			return new Rectangle(x*gridX,y*gridY,gridX,gridY);
+			return new Rectangle(x*(gridX+spacingX),y*(gridY+spacingY),gridX,gridY);
 		}
 	}
 }
