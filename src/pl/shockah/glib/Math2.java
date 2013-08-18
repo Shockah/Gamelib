e09ea0555f5a05b40a7c6a60d840fdf1e4ace85a
shockah <lolshockah@gmail.com>
2013-08-18 16:36:42 +0200

    Changes to Math2

--- src/pl/shockah/glib/Math2.java
+++ src/pl/shockah/glib/Math2.java
@@ -6,10 +6,10 @@ public final class Math2 {
 	}
 	
 	public static double ldirX(double dist, double angle) {
-		return -Math.cos(Math.toRadians(angle+180))*dist;
+		return -Math.cos(Math.toRadians(angle+180d))*dist;
 	}
 	public static double ldirY(double dist, double angle) {
-		return Math.sin(Math.toRadians(angle+180))*dist;
+		return Math.sin(Math.toRadians(angle+180d))*dist;
 	}
 	
 	public static double frac(double value) {
@@ -18,56 +18,74 @@ public final class Math2 {
 		return (value-Math.floor(value))*sign;
 	}
 	
-	public static int minI(int... values) {
-		int min = values[0];
-		for (int i = 1; i < values.length; i++) if (values[i] < min) min = values[i];
-		return min;
-	}
-	public static float minF(float... values) {
-		float min = values[0];
-		for (int i = 1; i < values.length; i++) if (values[i] < min) min = values[i];
-		return min;
-	}
-	public static double minD(double... values) {
+	public static double min(double... values) {
 		double min = values[0];
 		for (int i = 1; i < values.length; i++) if (values[i] < min) min = values[i];
 		return min;
 	}
+	public static float min(float... values) {
+		float min = values[0];
+		for (int i = 1; i < values.length; i++) if (values[i] < min) min = values[i];
+		return min;
+	}
+	public static long min(long... values) {
+		long min = values[0];
+		for (int i = 1; i < values.length; i++) if (values[i] < min) min = values[i];
+		return min;
+	}
+	public static int min(int... values) {
+		int min = values[0];
+		for (int i = 1; i < values.length; i++) if (values[i] < min) min = values[i];
+		return min;
+	}
 	
-	public static int maxI(int... values) {
-		int max = values[0];
-		for (int i = 1; i < values.length; i++) if (values[i] > max) max = values[i];
-		return max;
-	}
-	public static float maxF(float... values) {
-		float max = values[0];
-		for (int i = 1; i < values.length; i++) if (values[i] > max) max = values[i];
-		return max;
-	}
-	public static double maxD(double... values) {
+	public static double max(double... values) {
 		double max = values[0];
 		for (int i = 1; i < values.length; i++) if (values[i] > max) max = values[i];
 		return max;
 	}
+	public static float max(float... values) {
+		float max = values[0];
+		for (int i = 1; i < values.length; i++) if (values[i] > max) max = values[i];
+		return max;
+	}
+	public static long max(long... values) {
+		long max = values[0];
+		for (int i = 1; i < values.length; i++) if (values[i] > max) max = values[i];
+		return max;
+	}
+	public static int max(int... values) {
+		int max = values[0];
+		for (int i = 1; i < values.length; i++) if (values[i] > max) max = values[i];
+		return max;
+	}
 	
-	public static int limitI(int value, int min, int max) {
+	public static double limit(double value, double min, double max) {
 		return Math.min(Math.max(value,min),max);
 	}
-	public static int limitI(int value, int limit) {
+	public static float limit(float value, float min, float max) {
+		return Math.min(Math.max(value,min),max);
+	}
+	public static long limit(long value, long min, long max) {
+		return Math.min(Math.max(value,min),max);
+	}
+	public static int limit(int value, int min, int max) {
+		return Math.min(Math.max(value,min),max);
+	}
+	
+	public static double limit(double value, double limit) {
 		limit = Math.abs(limit);
 		return Math.min(Math.max(value,-limit),limit);
 	}
-	public static float limitF(float value, float min, float max) {
-		return Math.min(Math.max(value,min),max);
-	}
-	public static float limitF(float value, float limit) {
+	public static float limit(float value, float limit) {
 		limit = Math.abs(limit);
 		return Math.min(Math.max(value,-limit),limit);
 	}
-	public static double limitD(double value, double min, double max) {
-		return Math.min(Math.max(value,min),max);
+	public static long limit(long value, long limit) {
+		limit = Math.abs(limit);
+		return Math.min(Math.max(value,-limit),limit);
 	}
-	public static double limitD(double value, double limit) {
+	public static int limit(int value, int limit) {
 		limit = Math.abs(limit);
 		return Math.min(Math.max(value,-limit),limit);
 	}
