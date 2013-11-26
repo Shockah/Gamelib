3ac7fcbc9c692e6e6c62e7c5d90ad6455da4ef93
shockah <lolshockah@gmail.com>
2013-11-26 22:53:53 +0100

    Stencil.KeepWithDrawing,DropWithDrawing

--- src/pl/shockah/glib/gl/Stencil.java
+++ src/pl/shockah/glib/gl/Stencil.java
@@ -17,6 +17,18 @@ public abstract class Stencil {
 				glStencilOp(GL_KEEP,GL_REPLACE,GL_REPLACE);
 			}
 		},
+		KeepWithDrawing = new Stencil(){
+			public void phaseStencil(Graphics g) {
+				g.clear(Color.TransparentBlack,false,true);
+				glEnable(GL_STENCIL_TEST);
+				glStencilFunc(GL_NEVER,1,0xFF);
+				glStencilOp(GL_REPLACE,GL_KEEP,GL_KEEP);
+			}
+			public void phaseTest(Graphics g) {
+				glStencilFunc(GL_EQUAL,1,0xFF);
+				glStencilOp(GL_KEEP,GL_REPLACE,GL_REPLACE);
+			}
+		},
 		Drop = new Stencil(){
 			public void phaseStencil(Graphics g) {
 				super.phaseStencil(g);
@@ -25,6 +37,18 @@ public abstract class Stencil {
 			}
 			public void phaseTest(Graphics g) {
 				super.phaseTest(g);
+				glStencilFunc(GL_NOTEQUAL,1,0xFF);
+				glStencilOp(GL_KEEP,GL_KEEP,GL_REPLACE);
+			}
+		},
+		DropWithDrawing = new Stencil(){
+			public void phaseStencil(Graphics g) {
+				g.clear(Color.TransparentBlack,false,true);
+				glEnable(GL_STENCIL_TEST);
+				glStencilFunc(GL_NEVER,1,0xFF);
+				glStencilOp(GL_REPLACE,GL_KEEP,GL_KEEP);
+			}
+			public void phaseTest(Graphics g) {
 				glStencilFunc(GL_NOTEQUAL,1,0xFF);
 				glStencilOp(GL_KEEP,GL_KEEP,GL_REPLACE);
 			}
