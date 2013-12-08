eb51ed2d900fa65a7c759711dbbf4bc8076b1828
shockah <lolshockah@gmail.com>
2013-12-08 15:53:12 +0100

    Entity.created()/destroyed()

--- src/pl/shockah/glib/logic/Entity.java
+++ src/pl/shockah/glib/logic/Entity.java
@@ -5,25 +5,29 @@ import pl.shockah.glib.geom.vector.Vector2d;
 
 public abstract class Entity {
 	public Vector2d pos = new Vector2d();
-	private boolean firstTick = true;
+	private boolean firstTick = true, created = false, destroyed = false;
 	
 	protected Entity() {}
 	
 	public final void create() {create(null);}
 	public final void create(double x, double y) {create(new Vector2d(x,y));}
 	public void create(Vector2 pos) {
+		created = true;
 		if (pos != null) this.pos.set(pos);
 		firstTick = true;
 		Game.me.entitiesAdd.add(this);
 		onCreate();
 	}
 	protected void onCreate() {}
+	public final boolean created() {return created;}
 	
 	public void destroy() {
 		onDestroy();
 		Game.me.entitiesRemove.add(this);
+		destroyed = true;
 	}
 	protected void onDestroy() {}
+	public final boolean destroyed() {return destroyed;}
 	
 	public final void update() {
 		firstUpdate();
