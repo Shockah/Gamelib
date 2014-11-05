5e18f0a26e220a1c1f5aa0f16196d272910857fd
Michał Dolaś <michal.dolas@appdate.pl>
2014-11-05 13:21:23 +0100

    Updated to pl.shockah

--- src/pl/shockah/glib/gl/Shader.java
+++ src/pl/shockah/glib/gl/Shader.java
@@ -1,9 +1,6 @@
 package pl.shockah.glib.gl;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.ARBFragmentShader.*;
-import static org.lwjgl.opengl.ARBShaderObjects.*;
-import static org.lwjgl.opengl.ARBVertexShader.*;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -13,8 +10,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.HashMap;
 import java.util.Map;
+import org.lwjgl.opengl.ARBFragmentShader;
+import org.lwjgl.opengl.ARBShaderObjects;
+import org.lwjgl.opengl.ARBVertexShader;
 import pl.shockah.FileIO;
-import pl.shockah.IDoable;
+import pl.shockah.func.Action1;
 import pl.shockah.glib.LoadableProcessor;
 import pl.shockah.glib.geom.vector.Vector2d;
 import pl.shockah.glib.geom.vector.Vector2f;
@@ -43,24 +43,24 @@ public class Shader {
 			fragment = sb.toString()+fragment;
 		}
 		
-		int shaderVertex = createShader(GL_VERTEX_SHADER_ARB,vertex);
-		int shaderFragment = createShader(GL_FRAGMENT_SHADER_ARB,fragment);
+		int shaderVertex = createShader(ARBVertexShader.GL_VERTEX_SHADER_ARB,vertex);
+		int shaderFragment = createShader(ARBFragmentShader.GL_FRAGMENT_SHADER_ARB,fragment);
 		if (shaderVertex == 0 || shaderFragment == 0) {
 			throw new IOException("Couldn't create shader.");
 		}
 		
-		int sdrId = glCreateProgramObjectARB();
+		int sdrId = ARBShaderObjects.glCreateProgramObjectARB();
 		
-		glAttachObjectARB(sdrId,shaderVertex);
-		glAttachObjectARB(sdrId,shaderFragment);
+		ARBShaderObjects.glAttachObjectARB(sdrId,shaderVertex);
+		ARBShaderObjects.glAttachObjectARB(sdrId,shaderFragment);
 		
-		glLinkProgramARB(sdrId);
-		if (glGetObjectParameteriARB(sdrId,GL_OBJECT_LINK_STATUS_ARB) == GL_FALSE) {
+		ARBShaderObjects.glLinkProgramARB(sdrId);
+		if (ARBShaderObjects.glGetObjectParameteriARB(sdrId,ARBShaderObjects.GL_OBJECT_LINK_STATUS_ARB) == GL_FALSE) {
 			throw new IOException("Couldn't create shader.");
 		}
 		
-		glValidateProgramARB(sdrId);
-		if (glGetObjectParameteriARB(sdrId,GL_OBJECT_VALIDATE_STATUS_ARB) == GL_FALSE) {
+		ARBShaderObjects.glValidateProgramARB(sdrId);
+		if (ARBShaderObjects.glGetObjectParameteriARB(sdrId,ARBShaderObjects.GL_OBJECT_VALIDATE_STATUS_ARB) == GL_FALSE) {
 			throw new IOException("Couldn't create shader.");
 		}
 		
@@ -70,18 +70,18 @@ public class Shader {
 		if (code == null) return 0;
 		int id = 0;
 		try {
-			id = glCreateShaderObjectARB(type);
+			id = ARBShaderObjects.glCreateShaderObjectARB(type);
 			if (id == 0) throw new IOException("Couldn't create shader.");
 			
-			glShaderSourceARB(id,code);
-			glCompileShaderARB(id);
+			ARBShaderObjects.glShaderSourceARB(id,code);
+			ARBShaderObjects.glCompileShaderARB(id);
 			
-			if (glGetObjectParameteriARB(id,GL_OBJECT_COMPILE_STATUS_ARB) == GL_FALSE) {
+			if (ARBShaderObjects.glGetObjectParameteriARB(id,ARBShaderObjects.GL_OBJECT_COMPILE_STATUS_ARB) == GL_FALSE) {
 				throw new IOException("Couldn't create shader.");
 			}
 			return id;
 		} catch (Exception e) {
-			if (id != 0) glDeleteObjectARB(id);
+			if (id != 0) ARBShaderObjects.glDeleteObjectARB(id);
 			throw new IOException("Couldn't create shader.",e);
 		}
 	}
@@ -110,7 +110,7 @@ public class Shader {
 		
 		Integer loc = cacheUniforms.get(name);
 		if (loc == null) {
-			loc = glGetUniformLocationARB(sdrId,name);
+			loc = ARBShaderObjects.glGetUniformLocationARB(sdrId,name);
 			cacheUniforms.put(name,loc);
 		}
 		
@@ -122,13 +122,13 @@ public class Shader {
 	public void setUniform(String name, float f) {
 		Shader old = GL.boundShader();
 		if (!equals(old)) GL.bind(this);
-		glUniform1fARB(uniformLocation(name),f);
+		ARBShaderObjects.glUniform1fARB(uniformLocation(name),f);
 		if (!equals(old)) GL.bind(old);
 	}
 	public void setUniform(String name, int i) {
 		Shader old = GL.boundShader();
 		if (!equals(old)) GL.bind(this);
-		glUniform1iARB(uniformLocation(name),i);
+		ARBShaderObjects.glUniform1iARB(uniformLocation(name),i);
 		if (!equals(old)) GL.bind(old);
 	}
 	
@@ -138,21 +138,21 @@ public class Shader {
 	public void setUniform(String name, float f1, float f2) {
 		Shader old = GL.boundShader();
 		if (!equals(old)) GL.bind(this);
-		glUniform2fARB(uniformLocation(name),f1,f2);
+		ARBShaderObjects.glUniform2fARB(uniformLocation(name),f1,f2);
 		if (!equals(old)) GL.bind(old);
 	}
 	public void setUniform(String name, Vector2i v) {setUniform(name,v.x,v.y);}
 	public void setUniform(String name, int i1, int i2) {
 		Shader old = GL.boundShader();
 		if (!equals(old)) GL.bind(this);
-		glUniform2iARB(uniformLocation(name),i1,i2);
+		ARBShaderObjects.glUniform2iARB(uniformLocation(name),i1,i2);
 		if (!equals(old)) GL.bind(old);
 	}
 	
-	public void doWith(IDoable<Shader> ido) {
+	public void doWith(Action1<Shader> func) {
 		Shader old = GL.boundShader();
 		if (!equals(old)) GL.bind(this);
-		ido.doWith(this);
+		func.f(this);
 		if (!equals(old)) GL.bind(old);
 	}
 	
